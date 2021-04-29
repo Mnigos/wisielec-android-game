ca484fa169f042d55691c265c86709e2792b10ab
MoneyIgos <makowskiigor@gmail.com>
2021-04-29 11:14:54 +0200

    feat(MainActivity): add intent to GameActivity in btnPlay'

--- app/src/main/java/com/example/wisielec/MainActivity.kt
+++ app/src/main/java/com/example/wisielec/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.wisielec
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnPlay = findViewById<Button>(R.id.btnPlay)
+
+        btnPlay.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
