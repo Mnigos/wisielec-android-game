1a9f125606f3b8e4803a11df09b76833deb448d2
MoneyIgos <makowskiigor@gmail.com>
2021-04-29 12:33:03 +0200

    refactor(MainActivity): add input for words

--- app/src/main/java/com/example/wisielec/MainActivity.kt
+++ app/src/main/java/com/example/wisielec/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.wisielec
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +11,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnPlay = findViewById<Button>(R.id.btnPlay)
+        val btnPlay = findViewById<Button>(R.id.btn_play)
+        val btnEnd = findViewById<Button>(R.id.btn_end)
+        val editWord = findViewById<EditText>(R.id.edt_word)
 
         btnPlay.setOnClickListener {
             val intent = Intent(this, GameActivity::class.java)
+            val word = editWord.text.toString()
+            editWord.text = null
+            intent.putExtra("key", word)
             startActivity(intent)
+        }
+
+        btnEnd.setOnClickListener{
+            finishAffinity()
         }
     }
 }
